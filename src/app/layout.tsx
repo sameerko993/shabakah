d5a9319883a1c16850942a6edad7a49704bb4193
sam993 <sameer_haddad2000@yahoo.com>
2025-09-06 23:04:35 -0700

    fix: valid RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,16 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+import "./globals.css"
+import type { Metadata } from "next"
+import { ReactNode } from "react"
+
+export const metadata: Metadata = {
+  title: "Shabakah",
+  description: "Sports website",
+}
+
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body{children}</body>
+      <body>{children}</body>
     </html>
   )
 }
